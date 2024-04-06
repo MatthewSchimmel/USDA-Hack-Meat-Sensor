f8a85060f2e744d7aff292b84ac4c65368513ade
mattshim <matt.schimmel@tutanota.com>
2024-04-05 21:45:15 -0600

    adds the file and jpg-hsv conversion

---
+++ Main.py
@@ -0,0 +1,5 @@
+import cv2 as cv
+# using https://www.tutorialspoint.com/how-to-convert-an-rgb-image-to-hsv-image-using-opencv-python
+# TODO: create a while loop : while(there are more image files to parse)
+image = cv.imread('dog.jpg')
+print(image)
